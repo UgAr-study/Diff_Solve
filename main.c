d4aa78a18d9f6d4fa79822b7788c45615d602a74
UgAr-study <55754822+UgAr-study@users.noreply.github.com>
2019-10-20 01:27:01 +0300

    to rebuild
    он забивает не все элементы V, из-за этого косяки с другими элементами, тк он ведь прибавляет к предыдущему (хотя предыдущий ведь должен быть заполнен, тк нулевой он вообще не трогает, но возможно влияет на Y) надо подумать еще. Почему-то на концах производные сильно различаются. (размер V надо на 1 увеличить)

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <assert.h>
 
-double k = 0.224;
+// for P = 23.3H, a = 1.88 cm, b = 1.08 cm, E = 10 * 10^9
+double k = 0.583;
 //double step = 0.01;
 //double Low = 0;
 //double High = 48.08;
@@ -34,46 +35,49 @@ int main()
     printf("\nInput the number of points, you want to know (from x = 0): ");
     scanf("%d", &num);
 
-    int V_size = X_max / step;
-    printf("%d", V_size);
+    int V_size = (int)(X_max / step);
+    printf("%d\n", V_size);
     assert (num <= V_size);
 
     double* V = (double*) calloc (V_size + 1, sizeof(double));
 
-    int Vo = X_dY /step;
+    int Vo = (int)(X_dY /step);
     V[Vo] = dY;
     printf("%lg\n", V[Vo]);
 
     for (double x = X_dY; x - step >= 0; x -= step){
-        int i_pred = (x - step) / step;
-        int i_next = x / step;
-        //if (i == 0) break;
-        printf("func returned %lg\n", func(V[i_next], x));
+        int i_pred = (int)((x - step) / step);
+        int i_next = (int)(x / step);
+        //printf("func returned %lg\n", func(V[i_next], x));
         V[i_pred] = V[i_next] - step * func(V[i_next], x);
     }
 
-    for (double x = X_dY; x + step <= X_max; x += step){
+    for (double x = X_dY; x / step + 1 < V_size; x += step){
 
-        int i_pred = x / step;
-        int i_next = (x + step) / step;
-        //if (i == 0) break;
+        int i_pred = (int)(x / step);
+        int i_next = (int)((x + step) / step);
         //printf("func returned %lg\n", func(V[i_pred], x));
         V[i_next] = V[i_pred] + step * func(V[i_pred], x);
     }
 
     double* Y_val = (double*) calloc (V_size, sizeof(double));
 
-    for (double x = X_Y; x - step >= 0; x -= step ){
+    for(int i = 0; i < V_size; i++)
+        printf("V[%d] = %lg\n", i, V[i]);
+//    for(int i = 0; i < V_size; i++)
+//        Y_val[i] = 0.777;
+
+    for (double x = X_Y; x - step >= 0; x -= step){
         //printf("\nI'm in 'for' for Y down");
-        int i_pred = (x - step) / step;
-        int i_next = x / step;
+        int i_pred = (int) ((x - step) / step);
+        int i_next = (int) (x / step);
         Y_val[i_pred] = Y_val[i_next] - step * V[i_pred];
     }
 
     for (double x = X_Y; x + step <= X_max; x += step){
         //printf("\nI'm in 'for' for Y up");
-        int i_pred = x / step;
-        int i_next = (x + step) / step;
+        int i_pred = (int) (x / step);
+        int i_next = (int) ((x + step) / step);
         Y_val[i_next] = Y_val[i_pred] + step * V[i_pred];
     }
 
